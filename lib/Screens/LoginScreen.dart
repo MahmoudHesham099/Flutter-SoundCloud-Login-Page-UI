612a0865dd78b50ad30236ebc1829a70cf4b267d
hesham <17p6020@eng.asu.edu.eg>
2020-10-18 14:14:26 +0200

    done

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -8,6 +10,149 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: ListView(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height - 35,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height / 3,
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                const Color(0xFFf79810),
+                                const Color(0xFFf8310e),
+                              ],
+                              begin: const FractionalOffset(0.0, 0.0),
+                              end: const FractionalOffset(0.0, 1.0),
+                              stops: [0.0, 1.0],
+                              tileMode: TileMode.clamp,
+                            ),
+                            borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(70),
+                              bottomLeft: Radius.circular(70),
+                            )),
+                      ),
+                      Positioned(
+                        top: 0,
+                        bottom: 0,
+                        right: 0,
+                        left: 0,
+                        child: Padding(
+                          padding: const EdgeInsets.all(50),
+                          child: Image.asset('assets/logo.png'),
+                        ),
+                      )
+                    ],
+                  ),
+                  Text(
+                    'Create Account',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                              hintText: 'Email',
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              )),
+                          onChanged: (value) {
+                            print(value);
+                          },
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              hintText: 'Password',
+                              suffixIcon: Icon(Icons.remove_red_eye),
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              )),
+                          onChanged: (value) {
+                            print(value);
+                          },
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          height: 40,
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(3),
+                          ),
+                          child: Material(
+                            borderRadius: BorderRadius.circular(3),
+                            color: Colors.grey[300],
+                            child: MaterialButton(
+                              child: Text(
+                                'Sign Up with Email',
+                                textAlign: TextAlign.center,
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 15),
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          child: SignInButton(
+                            Buttons.FacebookNew,
+                            text: 'Sign Up with Facebook',
+                            onPressed: () {},
+                          ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          child: SignInButton(
+                            Buttons.GoogleDark,
+                            text: 'Sign Up with Google',
+                            onPressed: () {},
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text(
+                    'already have an account',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
